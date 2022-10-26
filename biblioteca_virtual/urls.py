eab12cfbd34f495d38d6113910f75b505b1adae8
Fátima <fsarvi@fi.uba.ar>
2022-10-26 12:46:36 -0300

    sinopsis parametrizada, items agregados a  contexto lista libros

--- biblioteca_virtual/urls.py
+++ biblioteca_virtual/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name= 'index'),
-    path('sinopsis',views.sinopsis, name= 'sinopsis')
+    path('sinopsis/<int:id>',views.sinopsis, name= 'sinopsis')
 ]
--- biblioteca_virtual/views.py
+++ biblioteca_virtual/views.py
@@ -4,27 +4,43 @@ from numpy import true_divide
 def index(request):
 ######################################provisorio#####################################
     """ lista provisoria hasta que podamos implementar la base de datos """
-    lista_libros = [["El intercambio","Lorem ipsum consectetuer adipiscing","pic11",True],
-    ["Dune","Lorem ipsum consectetuer adipiscing","pic12",False,True],
-    ["El gato negro","Lorem ipsum consectetuer adipiscing","pic13"],
-    ["Perdida","Lorem ipsum consectetuer adipiscing","pic14",True,True],
-    ["El tunel","Lorem ipsum consectetuer adipiscing","pic15",False,True],
-    ["El lector de cadaveres","Lorem ipsum consectetuer adipiscing","pic16",True],
-    ["Memento","Lorem ipsum consectetuer adipiscing","pic22"],
-    ["Quan Minimum","Lorem ipsum consectetuer adipiscing","pic17",True],
-    ["Credula Postero","Lorem ipsum consectetuer adipiscing","pic18", True],
-    ["Carpe Diem","Lorem ipsum consectetuer adipiscing","pic19"],
-    ["Expectatum","Lorem ipsum consectetuer adipiscing","pic21"],
-    ["Veni Vidi Vici","Lorem ipsum consectetuer adipiscing","pic20",False,True]]
+    lista_libros = [["El intercambio","Lorem ipsum consectetuer adipiscing","pic11",True,False,"1", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Dune","Lorem ipsum consectetuer adipiscing","pic12",False,True,"2", "Mezcla fascinante de aventura, misticismo, intrigas políticas y ecologismo, Dune se convirtió, desde el momento de su publicación, en un fenómeno de culto y en la mayor epopeya de ciencia-ficción de todos los tiempos.", "Agosto de 1965", "Frank Herbert", "Ciencia ficción/ Novela/ Literatura fantástica","Debolsillo", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["El gato negro","Lorem ipsum consectetuer adipiscing","pic13",False,False,"3", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Perdida","Lorem ipsum consectetuer adipiscing","pic14",True,True,"4", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["El tunel","Lorem ipsum consectetuer adipiscing","pic15",False,True,"5", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["El lector de cadaveres","Lorem ipsum consectetuer adipiscing","pic16",True, False,"6", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Memento","Lorem ipsum consectetuer adipiscing","pic22",False,False,"7", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Quan Minimum","Lorem ipsum consectetuer adipiscing","pic17",True, False,"8", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Credula Postero","Lorem ipsum consectetuer adipiscing","pic18", True, False,"9", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Carpe Diem","Lorem ipsum consectetuer adipiscing","pic19",False,False,"10", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Expectatum","Lorem ipsum consectetuer adipiscing","pic21",False,False,"11", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Veni Vidi Vici","Lorem ipsum consectetuer adipiscing","pic20",False,True,"12", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"]]
 
     libros = {"titulos" : lista_libros}
 ######################################################################################    
     return render(request, 'index.html', libros)
 
 
-def sinopsis(request):
+def sinopsis(request, id):
 ######################################provisorio#####################################
     """ listas provisorias hasta que podamos implementar la base de datos """
+    libros = [["El intercambio","Lorem ipsum consectetuer adipiscing","pic11",True,False,"1", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Dune","Lorem ipsum consectetuer adipiscing","pic12",False,True,"2", "Mezcla fascinante de aventura, misticismo, intrigas políticas y ecologismo, Dune se convirtió, desde el momento de su publicación, en un fenómeno de culto y en la mayor epopeya de ciencia-ficción de todos los tiempos.", "Agosto de 1965", "Frank Herbert", "Ciencia ficción/ Novela/ Literatura fantástica","Debolsillo", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["El gato negro","Lorem ipsum consectetuer adipiscing","pic13",False,False,"3", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Perdida","Lorem ipsum consectetuer adipiscing","pic14",True,True,"4", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["El tunel","Lorem ipsum consectetuer adipiscing","pic15",False,True,"5", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["El lector de cadaveres","Lorem ipsum consectetuer adipiscing","pic16",True, False,"6", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Memento","Lorem ipsum consectetuer adipiscing","pic22",False,False,"7", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Quan Minimum","Lorem ipsum consectetuer adipiscing","pic17",True, False,"8", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Credula Postero","Lorem ipsum consectetuer adipiscing","pic18", True, False,"9", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Carpe Diem","Lorem ipsum consectetuer adipiscing","pic19",False,False,"10", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Expectatum","Lorem ipsum consectetuer adipiscing","pic21",False,False,"11", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"],
+    ["Veni Vidi Vici","Lorem ipsum consectetuer adipiscing","pic20",False,True,"12", "El gato negro y otros relatos es una recopilacion de los cuentos mas notables de Poe, en los que destaca El gato negro, William Wilson y La carta robada", "19 de agosto, 1843", "Edgar Allan Poe", "Horror / Psicologico / Policial","Letras de Sopa", "Perturbadora, atrapante, magnifica", "El Canillita Times"]]
+
+    libro_seleccionado = libros[id - 1]
+    print(id)
+    print(libro_seleccionado)
     lista_libros_relacionados = [
     ["El buen hijo","buenhijo"],
     ["Reina del grito","reinagrito"],
@@ -44,6 +60,6 @@ def sinopsis(request):
     ["Juan Manuel Fangio", "voluptatibus aliquid quos nemo aut asperiores iste nostrum possimus iusto, cum velit fugit ad tempore vitae labore", 5],
     ["Ernesto Sabato", "nesciunt repellendus culpa fugiat reiciendis, magni voluptate, distinctio dicta. Sit laboriosam neque nemo eveniet atque nulla.", 5]]
 
-    libros_y_usuarios = {"usuarios":lista_usuarios,"relacionados":lista_libros_relacionados}
+    libros_y_usuarios = {"usuarios":lista_usuarios,"relacionados":lista_libros_relacionados, "libro_seleccionado": libro_seleccionado}
 ######################################################################################
     return render(request,'sinopsis.html',libros_y_usuarios)
