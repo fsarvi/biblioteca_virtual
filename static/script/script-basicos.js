5ec0387b770e31aba9b96e17399f2e037ec9bc49
Matias Barrios <45365752+matiasbarriosled@users.noreply.github.com>
2022-11-16 17:21:38 -0300

    script con funcion desplegar y cerrar
    la funcion desplegar quita el 100% de padding y la funcion cerrar se la añade para ocultarlo con el overflow

---
+++ static/script/script-basicos.js
@@ -0,0 +1,7 @@
+function desplegarBuscador(){
+    document.getElementById("buscador-desplegable").style.padding="0px"
+}
+
+function cierreBuscador(){
+    document.getElementById("buscador-desplegable").style.padding="0 0 0 100%"
+}
